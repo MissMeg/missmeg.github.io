98b3d9a6cf0a7db3fda3f0c50b336702b18e7906
Megan Roberts <megmrob@gmail.com>
2018-09-28 16:26:10 -0500

    updating about section css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   //initial_modal button
+  $('#initial_modal_button').delay(7000).fadeIn('slow');
   $('#initial_modal_button').click(() => {
     $('#initial_modal').fadeOut('slow');
+    $('#about-header h1').addClass('animate_header');
+    $('#about-content').delay(2000).fadeIn('slow');
+    $('#about-text').delay(2000).fadeIn('slow');
+    $('#about-favs').delay(2000).fadeIn('slow');
   });
 
-  
+
 });
